aaffb4506d0b411895bbe483a989c8a60058fccc
scribe <scribe@2a703577-9a0d-0410-ac02-84028a1de3c1>
2007-02-21 14:05:45 +0000

    mod_mobile Update : 三段顯示模式
    git-svn-id: http://svn.openfoundry.org/pixmicat/Modules@337 2a703577-9a0d-0410-ac02-84028a1de3c1

--- mod_mobile/mod_mobile.php
+++ mod_mobile/mod_mobile.php
@@ -5,13 +5,15 @@ By: scribe
 */
 
 class mod_mobile{
-	var $THREADLIST_NUMBER;
+	var $THREADLIST_NUMBER, $thisPage, $displayMode;
 
 	function mod_mobile(){
 		global $PMS;
 		$PMS->hookModuleMethod('ModulePage', 'mod_mobile'); // 向系統登記模組專屬獨立頁面
 
 		$this->THREADLIST_NUMBER = 10; // 一頁顯示列表個數
+		$this->thisPage = PMS::getModulePageURL('mod_mobile'); // 基底位置
+		$this->displayMode = isset($_COOKIE['dm']) ? $_COOKIE['dm'] : 's'; // 顯示模式 (s/m/l)
 	}
 
 	/* Get the name of module */
@@ -21,12 +23,12 @@ class mod_mobile{
 
 	/* Get the module version infomation */
 	function getModuleVersionInfo(){
-		return 'Pixmicat! Mobile Module v070220';
+		return 'Pixmicat! Mobile Module v070221';
 	}
 
 	/* 自動掛載：頂部連結列 */
 	function autoHookToplink(&$linkbar){
-		$linkbar .= '[<a href="'.PMS::getModulePageURL('mod_mobile').'">行動版</a>]'."\n";
+		$linkbar .= '[<a href="'.$this->thisPage.'">行動版</a>]'."\n";
 	}
 
 	/* 模組獨立頁面 */
@@ -35,15 +37,28 @@ class mod_mobile{
 
 		$err = ''; // 錯誤資訊
 		$res = isset($_GET['r']) ? intval($_GET['r']) : 0; // 回應編號
-		if($res !== 0){
-			$pageMax = $page = null; // Not in use
-			if(!$PIO->isThread($res)){ $err = 'Thread Not Found'; }
-			else{ $post = $PIO->fetchPosts($PIO->fetchPostList($res)); }
+		if(isset($_GET['dm'])){ // 是否進入設定模式
+			$ss = $ms = $ls = '';
+			switch($this->displayMode){
+				case 's': $ss = ' selected="selected"'; break;
+				case 'm': $ms = ' selected="selected"'; break;
+				case 'l': $ls = ' selected="selected"'; break;
+			}
+			$this->mobileHead($err, TITLE.' - 設定');
+			$err .= '<div>[<a href="'.$_SERVER['HTTP_REFERER'].'">回上頁</a>]<br/><form action="'.$this->thisPage.'" method="post">顯示模式:<br/><select name="dm"><option value="s"'.$ss.'>精簡 (無圖,裁字)</option><option value="m"'.$ms.'>一般 (無圖,全字)</option><option value="l"'.$ls.'>完整 (有圖,全字)</option></select><br/><input type="submit" value="儲存"/></form></div>';
+			$this->mobileFoot($err);
 		}else{
-			$pageMax = ceil($PIO->threadCount() / $this->THREADLIST_NUMBER) - 1; // 分頁最大編號
-			$page = isset($_GET['p']) ? intval($_GET['p']) : 0; // 目前所在分頁
-			if($page < 0 || $page > $pageMax){ $err = 'Page Out of Range'; } // $page 超過範圍
-			else{ $post = $PIO->fetchPosts($PIO->fetchThreadList($this->THREADLIST_NUMBER * $page, $this->THREADLIST_NUMBER, true)); } // 編號由大到小排序取出
+			if(isset($_POST['dm'])){ setCookie('dm', ($this->displayMode = $_POST['dm']), time()+604800); }
+			if($res !== 0){
+				$pageMax = $page = null; // Not in use
+				if(!$PIO->isThread($res)){ $err = 'Thread Not Found'; }
+				else{ $post = $PIO->fetchPosts($PIO->fetchPostList($res)); }
+			}else{
+				$pageMax = ceil($PIO->threadCount() / $this->THREADLIST_NUMBER) - 1; // 分頁最大編號
+				$page = isset($_GET['p']) ? intval($_GET['p']) : 0; // 目前所在分頁
+				if($page < 0 || $page > $pageMax){ $err = 'Page Out of Range'; } // $page 超過範圍
+				else{ $post = $PIO->fetchPosts($PIO->fetchThreadList($this->THREADLIST_NUMBER * $page, $this->THREADLIST_NUMBER, true)); } // 編號由大到小排序取出
+			}
 		}
 		if($err){ echo $err; }
 		else{
@@ -68,34 +83,36 @@ class mod_mobile{
 	/* 行動版內容 */
 	function mobileBody(&$dat, $pageMax, $page, $res, $post){
 		global $PIO, $FileIO;
-		$thisPage = PMS::getModulePageURL('mod_mobile'); // 基底位置
 		$post_count = count($post);
 		$dat .= '<div id="c">';
-		if($res !== 0){ $dat .= '[<a href="javascript:history.back();">回上頁</a>]<br/>'; }
+		if($res !== 0){ $dat .= '[<a href="'.$_SERVER['HTTP_REFERER'].'">回上頁</a>]<br/>'; }
 		// 逐步取資料
 		for($i = 0; $i < $post_count; $i++){
 			list($no, $sub, $name, $com, $tim, $ext) = array($post[$i]['no'], $post[$i]['sub'],$post[$i]['name'], $post[$i]['com'], $post[$i]['tim'], $post[$i]['ext']);
-			$reply1 = $res ? '<span class="s">'.$sub.'</span>' : '<a href="'.$thisPage.'&amp;r='.$no.'">'.$sub.'</a>'; // 回應模式連結
+			// 資料處理
+			if($this->displayMode==='s'){ $com = str_cut($com, 50); } // 取斷字元
+			$reply1 = $res ? '<span class="s">'.$sub.'</span>' : '<a href="'.$this->thisPage.'&amp;r='.$no.'">'.$sub.'</a>'; // 回應模式連結
 			$reply2 = $res ? '' : '<span class="w">('.($PIO->postCount($no) - 1).')</span>'; // 回應數
-			$img = $FileIO->imageExists($tim.'s.jpg') ? '<img src="'.$FileIO->getImageURL($tim.'s.jpg').'" alt="p"/><br/>' : '';
+			$img = ($this->displayMode==='l' && $FileIO->imageExists($tim.'s.jpg')) ? '<img src="'.$FileIO->getImageURL($tim.'s.jpg').'" alt="p"/><br/>' : '';
+			// 輸出
 			$dat .= '<div>'.$reply1.$reply2.'<span class="n">'.$name.'</span><br/>'.$img.$com.'</div><hr/>'."\n";
 		}
 		$dat .= '</div>';
 		if($page !== null){ // 分頁欄
 			$dat .= '<div id="p">';
-			if($page) $dat .= '<a href="'.$thisPage.'&amp;p='.($page - 1).'" accesskey="*">|&lt;</a> ';
+			if($page) $dat .= '<a href="'.$this->thisPage.'&amp;p='.($page - 1).'" accesskey="*">|&lt;</a> ';
 			for($i = 0; $i <= $pageMax; $i++){
 				if($i==$page) $dat .= '[<b>'.$i.'</b>] ';
-				else $dat .= '[<a href="'.$thisPage.'&amp;p='.$i.'" accesskey="'.$i.'">'.$i.'</a>] ';
+				else $dat .= '[<a href="'.$this->thisPage.'&amp;p='.$i.'" accesskey="'.$i.'">'.$i.'</a>] ';
 			}
-			if($page < $pageMax) $dat .= '<a href="'.$thisPage.'&amp;p='.($page + 1).'" accesskey="#">&gt;|</a>';
+			if($page < $pageMax) $dat .= '<a href="'.$this->thisPage.'&amp;p='.($page + 1).'" accesskey="#">&gt;|</a>';
 			$dat .= '</div>';
 		}
 	}
 
 	/* 行動版頁尾 */
 	function mobileFoot(&$dat){
-		$dat .= '<div>-Pixmicat!m-</div></body></html>';
+		$dat .= '<div id="f">-Pixmicat!m-<br><a href="#c">Top</a><br/><a href="'.$this->thisPage.'&amp;dm=set">顯示模式</a></div></body></html>';
 	}
 }
 ?>
