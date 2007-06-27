722a2805fd0f8cc4011b35959e225e23ae0459ba
scribe <scribe@2a703577-9a0d-0410-ac02-84028a1de3c1>
2007-06-27 09:11:27 +0000

    mod_recaptcha 釋出
    git-svn-id: http://svn.openfoundry.org/pixmicat/Modules@455 2a703577-9a0d-0410-ac02-84028a1de3c1

---
+++ mod_recaptcha/mod_recaptcha.php
@@ -0,0 +1,38 @@
+<?php
+/*
+mod_recaptcha.php
+*/
+include('recaptchalib.php'); // reCAPTCHA PHP Library
+
+class mod_recaptcha{
+	var $KEY_PUBLIC, $KEY_PRIVATE;
+
+	function mod_recaptcha(){
+		$this->KEY_PUBLIC = ''; // Public Key of this site
+		$this->KEY_PRIVATE = ''; // Private Key of this site
+	}
+
+	function getModuleName(){
+		return 'mod_recaptcha';
+	}
+
+	function getModuleVersionInfo(){
+		return 'reCAPTCHA 驗證圖像機制 v070615';
+	}
+
+	/* 在頁面附加 reCAPTCHA 圖像和功能 */
+	function autoHookPostForm(&$txt){
+		global $recaptcha_api_server;
+		$recaptcha_api_server = 'http://api.recaptcha.net';
+		$txt .= '<tr><th class="Form_bg">驗證碼</th><td>'.recaptcha_get_html($this->KEY_PUBLIC)."<small>(大小寫和符號需留意，兩個文字間用空白分隔)</small></td></tr>\n";
+	}
+
+	/* 在接收到送出要求後馬上檢查是否正確 */
+	function autoHookRegistBegin(&$name, &$email, &$sub, &$com, $upfileInfo, $accessInfo, $POST){
+		global $recaptcha_verify_server;
+		$recaptcha_verify_server = 'api-verify.recaptcha.net';
+		$resp = recaptcha_check_answer($this->KEY_PRIVATE, $_SERVER['REMOTE_ADDR'], $POST['recaptcha_challenge_field'], $POST['recaptcha_response_field']);
+		if(!$resp->is_valid){ error('reCAPTCHA 驗證碼錯誤！除大小寫須注意之外，標點符號及兩個單字都需輸入 (以空白分隔)'); } // 檢查
+	}
+}
+?>
