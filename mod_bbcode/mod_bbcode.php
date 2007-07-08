c85afb7f4a017c29364a933a7f097d91d80eea50
roytam1 <roytam1@2a703577-9a0d-0410-ac02-84028a1de3c1>
2007-07-08 04:09:09 +0000

    mod_bbcode: 加入 help page
    git-svn-id: http://svn.openfoundry.org/pixmicat/Modules@467 2a703577-9a0d-0410-ac02-84028a1de3c1

--- mod_bbcode/mod_bbcode.php
+++ mod_bbcode/mod_bbcode.php
@@ -1,8 +1,14 @@
 <?php
 class mod_bbcode{
 	var $ImgTagTagMode, $URLTagMode, $MaxURLCount, $URLTrapLog;
+	var $myPage;
 
 	function mod_bbcode(){
+		global $PMS;
+
+		$PMS->hookModuleMethod('ModulePage', 'mod_bbcode'); // 向系統登記模組專屬獨立頁面
+		$this->myPage = $PMS->getModulePageURL('mod_bbcode'); // 基底位置
+
 		$this->ImgTagTagMode = 1; // [img]標籤行為 (0:不轉換 1:無貼圖時轉換 2:常時轉換)
 		$this->URLTagMode = 1; // [url]標籤行為 (0:不轉換 1:正常)
 		$this->MaxURLCount = 2; // [url]標籤上限 (超過上限時標籤為陷阱標籤[寫入至$URLTrapLog])
@@ -18,7 +24,7 @@ class mod_bbcode{
 	}
 
 	function autoHookPostInfo(&$postinfo){
-		$postinfo .= '<li>可使用 BBCode</li>'."\n";
+		$postinfo .= "<li>可使用 <a href='".$this->myPage."' rel='_blank'>BBCode</a></li>\n";
 	}
 
 	function autoHookRegistBeforeCommit(&$name, &$email, &$sub, &$com, &$category, &$age, $dest, $resto, $imgWH){
@@ -46,28 +52,28 @@ class mod_bbcode{
 			if(preg_match_all("/\[url\](http|https|ftp)(:\/\/\S+?)\[\/url\]/si", $string, $matches, PREG_SET_ORDER)){
 				$urlcount+=count($matches);
 				foreach($matches as $submatches){
-					$string = @str_replace($submatches[0], "<a href=\"$submatches[1]$submatches[2]\" target=\"_blank\">$submatches[1]$submatches[2]</a>", $string);
+					$string = @str_replace($submatches[0], "<a href=\"$submatches[1]$submatches[2]\" rel=\"_blank\">$submatches[1]$submatches[2]</a>", $string);
 				}
 			}
 
 			if(preg_match_all("/\[url\](\S+?)\[\/url\]/si", $string, $matches, PREG_SET_ORDER)){
 				$urlcount+=count($matches);
 				foreach($matches as $submatches){
-					$string = @str_replace($submatches[0], "<a href=\"http://$submatches[1]\" target=\"_blank\">$submatches[1]</a>", $string);
+					$string = @str_replace($submatches[0], "<a href=\"http://$submatches[1]\" rel=\"_blank\">$submatches[1]</a>", $string);
 				}
 			}
 
 			if(preg_match_all("/\[url=(http|https|ftp)(:\/\/\S+?)\](.*?)\[\/url\]/si", $string, $matches, PREG_SET_ORDER)){
 				$urlcount+=count($matches);
 				foreach($matches as $submatches){
-					$string = @str_replace($submatches[0], "<a href=\"$submatches[1]$submatches[2]\" target=\"_blank\">$submatches[3]</a>", $string);
+					$string = @str_replace($submatches[0], "<a href=\"$submatches[1]$submatches[2]\" rel=\"_blank\">$submatches[3]</a>", $string);
 				}
 			}
 
-			if(preg_match_all("/\[url=(\S+?)\](\S+?)\[\/url\]/si", $string, $matches, PREG_SET_ORDER)){
+			if(preg_match_all("/\[url=(\S+?)\](.*?)\[\/url\]/si", $string, $matches, PREG_SET_ORDER)){
 				$urlcount+=count($matches);
 				foreach($matches as $submatches){
-					$string = @str_replace($submatches[0], "<a href=\"http://$submatches[1]\" target=\"_blank\">$submatches[2]</a>", $string);
+					$string = @str_replace($submatches[0], "<a href=\"http://$submatches[1]\" rel=\"_blank\">$submatches[2]</a>", $string);
 				}
 			}
 			$this->_URLExcced($urlcount);
@@ -93,6 +99,47 @@ class mod_bbcode{
 		  	  fclose($fh);
 		  	  error("[url]標籤超過上限");
 		}
+	}
+
+	function ModulePage(){
+		$dat='';$status='現時BBCode設定:<ul><li>[url]標籤行為 (0:不轉換 1:正常) - '.$this->URLTagMode.'</li><li>[url]標籤上限 (超過上限時標籤為陷阱標籤並寫入至記錄檔中) - '.$this->MaxURLCount.'</li><li>'._T('info_basic_urllinking').' '._T('info_0no1yes').' - '.AUTO_LINK.'</li><li>[img]標籤行為 (0:不轉換 1:無貼圖時轉換 2:常時轉換) - '.$this->ImgTagTagMode.'</li></ul>';
+		head($dat);
+		$dat.=<<<EOH
+$status
+BBCode 代碼包含一些標籤方便您快速的更改文字的基本形式. 這些可以分述如下: 
+<ul><li>要製作一份粗體文字可使用 <b>[b][/b]</b>, 例如: <br/><br/><b>[b]</b>哈囉<b>[/b]</b><br/><br/>會變成<b>哈囉</b><br/><br/></li>
+<li>要使用底線時, 可使用<b>[u][/u]</b>, 例如:<br/><br/><b>[u]</b>早安<b>[/u]</b><br/><br/>會變成<u>早安</u><br/><br/></li>
+<li>要斜體顯示時, 可使用 <b>[i][/i]</b>, 例如:<br/><br/>這個真是 <b>[i]</b>棒呆了!<b>[/i]</b><br/><br/>將會變成 這個真是 <i>棒呆了!</i></li></ul>
+
+要在您的文章中修改文字顏色及大小需要使用以下的標籤. 請注意, 顯示的效果視您的瀏覽器和系統而定: 
+<ul><li>更改文字色彩時, 可使用 <b>[color=][/color]</b>. 您可以指定一個可被辨識的顏色名稱(例如. red, blue, yellow, 等等.) 或是使用顏色編碼, 例如: #FFFFFF, #000000. 舉例來說, 要製作一份紅色文字您必須使用:<br/><br/><b>[color=red]</b>哈囉!<b>[/color]</b><br/><br/>或是<br/><br/><b>[color=#FF0000]</b>哈囉!<b>[/color]</b><br/><br/>都將顯示:<font color="red">哈囉!</font><br/><br/></li>
+<li>改變文字的大小也是使用類似的設定, 標籤為 <b>[s?][/s?]</b>. 起始值為 1 (細小) 到 7 為止 (巨大). 舉例說明:<br/><br/><b>[s1]</b>小不拉嘰<b>[/s1]</b><br/><br/>將會產生 <font size="1">小不拉嘰</font><br/><br/>當情形改變時:<br/><br/><b>[s7]</b>有夠大顆!<b>[/s7]</b><br/><br/>將會顯示 <font size="7">有夠大顆!</font></li></ul>
+
+可以結合不同的標籤功能: <br/>
+<ul><li>例如要吸引大家的注意時, 您可以使用:<br/><br/><b>[s5][color=red][b]</b>看我這兒!<b>[/b][/color][/s5]</b><br/><br/> 將會顯示出 <font size="5"><font color="red"><b>看我這兒!</b></font></font><br/>&nbsp;</li>
+<li>我們並不建議您顯示太多這類的文字! 但是這些還是由您自行決定. 在使用 BBCode 代碼時, 請記得要正確的關閉標籤, 以下就是錯誤的使用方式:<br/><br/><b>[b][u]</b>這是錯誤的示範<b>[/b][/u]</b></li></ul>
+
+如果您想要顯示一段程式代碼或是任何需要固定寬度的文字, 您必須使用 <b>[pre][/pre]</b> 標籤來包含這些文字, 例如:<br/><br/><b>[pre]</b>echo "這是代碼";<b>[/pre]</b><br/><br/>當您瀏覽時, 所有被 <b>[pre][/pre]</b> 標籤包含的文字格式都將保持不變.
+
+若一個完整的URL遵照此方式寫入至討論板，將會自動產生一個超連結連往該URL
+<ul><li>http://開頭的會自動成為超連結 (如果自動連結有啟用的話)</li>
+    <li>[url]可以做成一個超連結，請參考下例：<br/>
+                  <b>[url=http://php.s3.to]</b>按這裡<b>[/url]</b></li>
+    <li>下一個方式也有類似效果<br/>
+           <b>[url]</b>php.s3.to<b>[/url]</b>
+        <p>以上舉例說明中，自動產生了連結以刊登URL。使用者按下該連結將跳出一視窗。沒有"http://"是不能自動產生連結的。<br/>第二個方法裡可以省略"http://"。避免於URL中置入「"」記號，那可能會截斷網址。</p>
+    </li></ul>
+為了加上Email的連結，請按照以下方式刊登郵址: 
+<ul><li><b>[email]</b>php@php.4all.cc<b>[/email]</b></li>
+<li>下一個方式也可以做成一個Email連結<br/>
+<b>[email=php@php.4all.cc]</b>我的Email<b>[/email]</b></li>
+</ul>
+
+BBCode 代碼提供標籤在您的文章中顯示圖像. 使用前, 請記住兩件重要的事;  第一, 許多使用者並不樂於見到文章中有太多的圖片, 第二, 您的圖片必須是能在網路上顯示的 (例如: 不能是您電腦上的檔案, 除非您的電腦是台網路伺服器). 若要顯示圖像, 可以使用 <b>[img][/img]</b> 標籤並指定圖像連結網址,  例如:<br/><br/><b>[img]</b>http://www.google.com/intl/en_com/images/logo_plain.png<b>[/img]</b><br/><br/>如同在先前網址連結的說明一樣, 您也可以使用圖片網址超連結 <b>[url][/url]</b> 的標籤, 例如:<br/><br/><b>[url=http://www.google.com/][img]</b>http://www.google.com/intl/en_com/images/logo_plain.png<b>[/img][/url]</b><br/><br/>將產生:<br/><br/><a href="http://www.google.com/" rel="_blank"><img src="http://www.google.com/intl/en_com/images/logo_plain.png" alt="http://www.google.com/intl/en_com/images/logo_plain.png" border="0" /></a>
+<hr/>
+EOH;
+		foot($dat);
+		echo $dat;
 	}
 }
 ?>
