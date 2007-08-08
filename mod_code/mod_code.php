1caf26fc2f93a4a6045fb7c10ee1bc977fe3ce78
scribe <scribe@2a703577-9a0d-0410-ac02-84028a1de3c1>
2007-08-08 05:20:51 +0000

    針對 auto_link 行為更動作的多行修正
    git-svn-id: http://svn.openfoundry.org/pixmicat/Modules@504 2a703577-9a0d-0410-ac02-84028a1de3c1

--- mod_code/mod_code.php
+++ mod_code/mod_code.php
@@ -11,7 +11,7 @@ class mod_code{
 	}
 
 	function getModuleVersionInfo(){
-		return 'mod_code : dp.SyntaxHighlighter Embedded v070615';
+		return 'mod_code : dp.SyntaxHighlighter Embedded v070808';
 	}
 
 	function autoHookHead(&$dat){
@@ -22,19 +22,24 @@ class mod_code{
 _EOF_;
 		foreach($this->brushes as $b){ $dat .= '<script type="text/javascript" src="module/shBrush'.$b.'.js"></script>'."\n"; } // 載入刷子檔
 	}
-	
+
 	function autoHookPostInfo(&$postinfo){
 		$postinfo .= '<li>程式碼可使用 [code=類型][/code] 以 dp.SyntaxHighlighter 標亮 (<a href="http://code.google.com/p/syntaxhighlighter/wiki/Languages" rel="_blank">類型列表</a>)</li>'."\n";
 	}
 
+	function _textarea($s){
+		return '<textarea name="code" cols="10\" rows="10" class="'.$s[1].'">'.str_replace('<br />', '', $s[2]).'</textarea>';
+	}
+
 	function autoHookThreadPost(&$arrLabels, $post, $isReply){
 		extract($post);
-		$arrLabels['{$COM}'] = preg_replace('/\[code=(\w*?)\](.*?)\[\/code\]/ue', "'<textarea name=\"code\" cols=\"10\" rows=\"10\" class=\"$1\">'.str_replace('<br />', \"\n\", '$2').'</textarea>'", $arrLabels['{$COM}']);
+		if(strpos($arrLabels['{$COM}'], '[/code]')===false) return;
+
+		$arrLabels['{$COM}'] = preg_replace_callback('/\[code=(\w*?)\](.*?)\[\/code\]/us', array(&$this, '_textarea'), $arrLabels['{$COM}']);
 	}
 
 	function autoHookThreadReply(&$arrLabels, $post, $isReply){
-		extract($post);
-		$arrLabels['{$COM}'] = preg_replace('/\[code=(\w*?)\](.*?)\[\/code\]/ue', "'<textarea name=\"code\" cols=\"10\" rows=\"10\" class=\"$1\">'.str_replace('<br />', \"\n\", '$2').'</textarea>'", $arrLabels['{$COM}']);
+		$this->autoHookThreadPost($arrLabels, $post, $isReply);
 	}
 
 	function autoHookFoot(&$dat){
