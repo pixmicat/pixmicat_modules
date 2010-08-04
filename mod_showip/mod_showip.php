15e3acc870837c8fe7088da5a40ec2a952040184
roytam1 <roytam1@2a703577-9a0d-0410-ac02-84028a1de3c1>
2010-08-04 05:17:10 +0000

    - [mod_showip] 添加部份ISP的IP hack
    git-svn-id: http://svn.openfoundry.org/pixmicat/Modules@745 2a703577-9a0d-0410-ac02-84028a1de3c1

--- mod_showip/mod_showip.php
+++ mod_showip/mod_showip.php
@@ -39,8 +39,16 @@ class mod_showip{
 			$cctld = array('ac','ad','ae','af','ag','ai','al','am','an','ao','aq','ar','as','at','au','aw','ax','az','ba','bb','bd','be','bf','bg','bh','bi','bj','bm','bn','bo','br','bs','bt','bw','by','bz','ca','cc','cd','cf','cg','ch','ci','ck','cl','cm','cn','co','cr','cu','cv','cx','cy','cz','de','dj','dk','dm','do','dz','ec','ee','eg','er','es','et','eu','fi','fj','fk','fm','fo','fr','ga','gd','ge','gf','gg','gh','gi','gl','gm','gn','gp','gq','gr','gs','gt','gu','gw','gy','hk','hm','hn','hr','ht','hu','id','ie','il','im','in','io','iq','ir','is','it','je','jm','jo','jp','ke','kg','kh','ki','km','kn','kp','kr','kw','ky','kz','la','lb','lc','li','lk','lr','ls','lt','lu','lv','ly','ma','mc','md','me','mg','mh','mk','ml','mm','mn','mo','mp','mq','mr','ms','mt','mu','mv','mw','mx','my','mz','na','nc','ne','nf','ng','ni','nl','no','np','nr','nu','nz','om','pa','pe','pf','pg','ph','pk','pl','pn','pr','ps','pt','pw','py','qa','re','ro','rs','ru','rw','sa','sb','sc','sd','se','sg','sh','si','sk','sl','sm','sn','sr','st','su','sv','sy','sz','tc','td','tf','tg','th','tj','tk','tl','tm','tn','to','tr','tt','tv','tw','tz','ua','ug','uk','us','uy','uz','va','vc','ve','vg','vi','vn','vu','wf','ws','ye','za','zm','zw');
 			preg_match('/([\w\-]+)\.(\w+)$/',$post['host'],$parts);
 
-			if(preg_match('/on-nets$/',$parts[1])) { //on-nets hack
-				$parthost = '*-on-nets.com';
+			if(preg_match('/on-nets$/',$parts[1])) { // on-nets IP hack
+				if(preg_match('/(\d+)\-(\d+)-on-nets/',$parts[1],$ipparts))
+					$parthost = $ipparts[2].'.'.$ipparts[1].'.*.on-nets.com';
+				else
+					$parthost = '*-on-nets.com';
+			} elseif($parts[1] == 'hinet') { // hinet IP hack
+				if(preg_match('/^(\d+\-\d+)/',$post['host'],$ipparts))
+					$parthost = $ipparts[0].'-*.'.$parts[0];
+				else
+					$parthost = '*.'.$parts[0];
 			} else {
 				$lastpart = strtolower($parts[2]);
 				$isgtld = $this->_isgTLD($lastpart);
@@ -51,10 +59,17 @@ class mod_showip{
 							$iscctld = true;
 							preg_match('/([\w\-]+)\.([\w\-]+)\.(\w+)$/',$post['host'],$parts);
 							$isgtld = $this->_isgTLD($parts[2],array('ac','ad','co','ed','go','gr'.'lg','ne','or','ind','ltd','nic','plc','vet')); // '.co.uk' etc. are common
-							if($isgtld)
-								$parthost = '*.'.$parts[0];
-							else
+							if($isgtld) {
+								if($parts[1] == 'kbronet' || $parts[1] == 'seed' || $parts[1] == 'so-net' || $parts[1] == 'tfn') { // kbronet/seed/so-net/tfn IP hack
+									if(preg_match('/^(\d+\-\d+)/',$post['host'],$ipparts))
+										$parthost = $ipparts[0].'-*.'.$parts[0];
+									else
+										$parthost = '*.'.$parts[0];
+								} else 
+									$parthost = '*.'.$parts[0];
+							} else {
 								$parthost = '*.'.$parts[2].'.'.$parts[3];
+							}
 							break;
 						}
 					}
