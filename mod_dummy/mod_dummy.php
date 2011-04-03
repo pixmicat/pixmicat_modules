a45d9a3c7964bac5e961c8f37959909993c614e5
scribe <scribe@2a703577-9a0d-0410-ac02-84028a1de3c1>
2011-04-03 05:50:39 +0000

    mod_dummy - 加上註解，修正寫法。希望能夠讓更多人了解模組撰寫規則
    git-svn-id: http://svn.openfoundry.org/pixmicat/Modules@803 2a703577-9a0d-0410-ac02-84028a1de3c1

--- mod_dummy/mod_dummy.php
+++ mod_dummy/mod_dummy.php
@@ -1,25 +1,39 @@
 <?php
 class mod_dummy{
-	function mod_dummy(){
-		global $PMS;
-		$PMS->hookModuleMethod('ModulePage', 'mod_dummy');
+	var $SELF;
+
+	/**
+	 * 建構元
+	 */
+	function mod_dummy($PMS){
+		// 自建構元取出 $PMS 變數 (6th.Release 開始加入)
+		// 使用 hookModuleMethod 註冊 ModulePage，__CLASS__ 代表物件類別名稱 (本例為 mod_dummy)
+		$PMS->hookModuleMethod('ModulePage', __CLASS__);
+		// 取得 ModulePage URL，並存在變數裡
+		$this->SELF = $PMS->getModulePageURL(__CLASS__);
 	}
 
+	/**
+	 * 回傳模組名稱方法
+	 * 
+	 * @return 模組名稱。建議回傳格式: mod_xxx : 簡短註解
+	 */
 	function getModuleName(){
-		return 'mod_dummy';
+		return __CLASS__.' : 展示掛載點功能模組';
 	}
 
+	/**
+	 * 回傳模組版本號方法
+	 * 
+	 * @return 模組版本號。
+	 */
 	function getModuleVersionInfo(){
-		return 'mod_dummy : 展示掛載點功能模組';
+		return 'v110403';
 	}
 
-	function ModulePage(){
-		echo "Welcome to my world.";
-	}
-
+	// 以下為 autoHookXXX 的實驗，可以觀察到各常用掛載點是如何操作
 	function autoHookToplink(&$link){
-		global $PMS;
-		$link .= '[<a href="'.$PMS->getModulePageURL('mod_dummy').'">統計</a>]'."\n";
+		$link .= '[<a href="'.$this->SELF.'">統計</a>]'."\n";
 	}
 
 	function autoHookPostInfo(&$txt){
@@ -36,6 +50,15 @@ class mod_dummy{
 
 	function autoHookFoot(&$foot){
 		$foot .= '<span class="warn_txt2">本網站由 雙貓聯合站 提供資源，謹此致謝</span>'."\n";
+	}
+
+	/**
+	 * 模組頁面方法
+	 * 
+	 * 此方法為瀏覽 ModulePage URL 之後，程式會呼叫的方法。你可以在此印出屬於模組自己的內容，比如說設定項目，列表等等。
+	 */
+	function ModulePage(){
+		echo "Welcome to my world.";
 	}
 }
 ?>
