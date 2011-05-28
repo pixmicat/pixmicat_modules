306e6e14a96fa96ec4eef37a1627e685173f7bab
scribe <scribe@2a703577-9a0d-0410-ac02-84028a1de3c1>
2011-05-28 10:56:55 +0000

    - mod_imgsizelimit: 新增圖像最大寬高限制
    git-svn-id: http://svn.openfoundry.org/pixmicat/Modules@814 2a703577-9a0d-0410-ac02-84028a1de3c1

--- mod_imgsizelimit/mod_imgsizelimit.php
+++ mod_imgsizelimit/mod_imgsizelimit.php
@@ -4,30 +4,34 @@ mod_imgsizelimit.php
 */
 
 class mod_imgsizelimit{
-	var $MIN_W, $MIN_H, $MIN_FILESIZE;
+	var $MIN_W, $MIN_H, $MIN_FILESIZE, $MAX_W, $MAX_H;
 
 	function mod_imgsizelimit(){
 		$this->MIN_W = 320; // 最小寬度 (0為不限制)
 		$this->MIN_H = 240; // 最小高度 (0為不限制)
+		$this->MAX_W = 5000; // 最大寬度 (0為不限制)
+		$this->MAX_H = 5000; // 最大高度 (0為不限制)
 		$this->MIN_FILESIZE = 0; // 最小檔案大小 (位元組, 0為不限制)
 	}
 
 	function getModuleName(){
-		return 'mod_imgsizelimit : 限制圖像最小大小';
+		return 'mod_imgsizelimit : 限制圖像長寬大小';
 	}
 
 	function getModuleVersionInfo(){
-		return 'v100310';
+		return 'v110528';
 	}
 
 	function autoHookRegistBeforeCommit(&$name, &$email, &$sub, &$com, &$category, &$age, $dest, $isReply, $imgWH, &$status){
 		if( ($dest !== '') && (
 			($this->MIN_W && $this->MIN_W > $imgWH[2]) ||
 			($this->MIN_H && $this->MIN_H > $imgWH[3]) ||
+			($this->MAX_W && $imgWH[2] > $this->MAX_W) ||
+			($this->MAX_H && $imgWH[3] > $this->MAX_H) ||
 			($this->MIN_FILESIZE && $this->MIN_FILESIZE > filesize($dest)) )
 			) { 
 				unlink($dest);
-				error('圖像大小小於限制。');
+				error('圖像大小不符限制。');
 			}
 	}
 }
