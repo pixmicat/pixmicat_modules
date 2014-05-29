1bcf784b7c547160cdaeca5d91eda6268c7dca47
KennyL <apple@apples-MacBook-Pro.local>
2014-05-29 16:24:58 +0800

    mod_fblike 7th寫法 ＆BUGFIX

--- mod_fblike/mod_fblike.php
+++ mod_fblike/mod_fblike.php
@@ -1,6 +1,14 @@
 <?php
-class mod_fblike{
-	var $site, $url;
+class mod_fblike extends ModuleHelper {
+	private $site = TITLE;
+	private $url ;
+
+	public function __construct($PMS) {
+		parent::__construct($PMS);
+		$this->url =  fullURL().PHP_SELF.'?res=';
+
+	}
+
 	function mod_fblike(){
 		$this->site = TITLE;
 		$this->url = fullURL().PHP_SELF.'?res=';
@@ -11,11 +19,11 @@ class mod_fblike{
 	}
 
 	function getModuleVersionInfo(){
-		return 'v110102';
+		return 'v140529';
 	}
 
 	function autoHookHead(&$style, $isReply){
-		global $PIO;
+		$PIO = PMCLibrary::getPIOInstance();
 		if($isReply){
 			$p = $PIO->fetchPosts($isReply);
 			$sub = $p[0]['sub'];
@@ -28,7 +36,6 @@ _HERE_;
 	}
 
 	function autoHookThreadPost(&$arrLabels, $post, $isReply){
-		$arrLabels['{$REPLYBTN}'] .= '&nbsp;<iframe src="http://www.facebook.com/plugins/like.php?href='.urlencode($this->url.$post['no']).'&amp;layout=button_count&amp;width=90%anp;font=arial" scrolling="no" frameborder="0" allowTransparency="true" style="border: medium none; overflow: hidden; height: 20px; width: 90px; vertical-align: text-bottom;"></iframe>';
+		$arrLabels['{$REPLYBTN}'] .= '&nbsp;<iframe src="http://www.facebook.com/plugins/like.php?href='.urlencode($this->url.$post['no']).'&amp;layout=button_count&amp;width=90&amp;font=arial" scrolling="no" frameborder="0" allowTransparency="true" style="border: medium none; overflow: hidden; height: 20px; width: 90px; vertical-align: text-bottom;"></iframe>';
 	}
 }
-?>
