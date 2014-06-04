0be25e068a289c7d626584da3f4ccfc945ed0294
KennyL <apple@apples-MacBook-Pro.local>
2014-06-04 13:45:04 +0800

    Peformance Tweak

--- mod_pushpost/mod_pushpost.php
+++ mod_pushpost/mod_pushpost.php
@@ -28,12 +28,10 @@ class mod_pushpost extends ModuleHelper {
 	}
 
 	public function autoHookHead(&$txt, $isReply) {
-		//fast check if jquery exists
-		if (strpos($txt, 'jquery')===false){
-			$txt .=  '<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>';
-		}
-
-		$txt .= '<style type="text/css">.pushpost { background-color: #fff; font-size: 0.8em; padding: 10px; }</style>
+		$txt .= '<script type="text/javascript">
+window.jQuery || document.write("\x3Cscript src=\"//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js\">\x3C/script>");
+</script>
+<style type="text/css">.pushpost { background-color: #fff; font-size: 0.8em; padding: 10px; }</style>
 <script type="text/javascript">
 // <![CDATA[
 var lastpushpost=0;
