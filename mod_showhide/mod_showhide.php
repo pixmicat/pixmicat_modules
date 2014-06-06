3c1087da4bdf314275b8d32b4d7caceb8ba23273
Kenny <me@kennyforge.com>
2014-06-06 23:23:21 +0800

    [showhide] 7th 寫法。

--- mod_showhide/mod_showhide.php
+++ mod_showhide/mod_showhide.php
@@ -1,16 +1,16 @@
 <?php
-class mod_showhide{
-	function getModuleName(){
+class mod_showhide extends ModuleHelper {
+	public function getModuleName(){
 		return 'mod_showhide : 自由隱藏顯示討論串';
 	}
 
-	function getModuleVersionInfo(){
-		return '4th.Release.3 (v080519)';
+	public function getModuleVersionInfo(){
+		return '7th.Release.dev (v140606)';
 	}
 
-	function autoHookHead(&$txt, $isReply){
-		$txt .= '<script type="text/javascript" src="module/jquery-latest.pack.js"></script>
+	public function autoHookHead(&$txt, $isReply){
+		$txt .= '<script type="text/javascript">window.jQuery || document.write("\x3Cscript src=\"//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js\">\x3C/script>");</script> 
 <script type="text/javascript" src="module/mod_showhide.pack.js"></script>'."\n";
 	}
 }
-?>
+
