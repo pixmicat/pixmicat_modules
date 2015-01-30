a179fa444d7824d40e6b79a59367638de5f07be5
ben.wong <ben.wong@tfidm.com>
2015-01-30 15:02:07 +0800

    reseted, fixed problems in review, still tabs

--- mod_recaptcha_v2/mod_recaptcha_v2.php
+++ mod_recaptcha_v2/mod_recaptcha_v2.php
@@ -3,38 +3,39 @@
 mod_recaptcha_v2.php
 */
 require_once('recaptchalib.php'); // reCAPTCHA PHP Library
- 
-class mod_recaptcha_v2{
-        var $KEY_PUBLIC, $KEY_PRIVATE;
-        var $reCaptcha;
- 
-        function mod_recaptcha_v2(){
-                $this->KEY_PUBLIC = ''; // Public Key of this site
-                $this->KEY_PRIVATE = ''; // Private Key of this site
-                $this->reCaptcha = new ReCaptcha($this->KEY_PRIVATE);
-        }
- 
-        function getModuleName(){
-                return 'mod_recaptcha_v2 : reCAPTCHA v2';
-        }
- 
-        function getModuleVersionInfo(){
-                return '0.Alpha.0 (v150126)';
-        }
- 
-        function autoHookHead(string &$head, int $isReply){
-                $head.="<script src='https://www.google.com/recaptcha/api.js?hl=zh-TW'></script>";
-        }
- 
-        /* 在頁面附加 reCAPTCHA 功能 */
-        function autoHookPostForm(&$txt){
-                $txt .= '<tr><th class="Form_bg">驗證</th><td>'.'<div class="g-recaptcha" data-sitekey="'.$this->KEY_PUBLIC.'"></div>'.'</td></tr>\n';
-        }
- 
-        /* 在接收到送出要求後馬上檢查是否正確 */
-        function autoHookRegistBegin(&$name, &$email, &$sub, &$com, $upfileInfo, $accessInfo){
-                $resp = $this->reCaptcha->verifyResponse($_SERVER['REMOTE_ADDR'], $_POST['g-recaptcha-response']);
-                if($resp == null || !$resp->success){ error('reCAPTCHA failed！You are not acting like a human!'); } // 檢查
-        }
+
+class mod_recaptcha_v2 extends ModuleHelper {
+	private $KEY_PUBLIC = '';
+	private $KEY_PRIVATE = '';
+	private $reCaptcha;
+
+	public function __construct($PMS) {
+		parent::__construct($PMS);
+		$this->reCaptcha = new ReCaptcha($this->KEY_PRIVATE);
+	}
+
+	public function getModuleName(){
+		return 'mod_recaptcha_v2 : reCAPTCHA v2';
+	}
+
+	public function getModuleVersionInfo(){
+		return '7th.Alpha.0 (v150127)';
+	}
+
+	public function autoHookHead(&$head, $isReply){
+		$head.="<script src='https://www.google.com/recaptcha/api.js?hl=zh-TW'></script>";
+	}
+
+	/* 在頁面附加 reCAPTCHA 功能 */
+	public function autoHookPostForm(&$txt){
+		$txt .= '<tr><th class="Form_bg">驗證</th><td>'.'<div class="g-recaptcha" data-sitekey="'.$this->KEY_PUBLIC.'"></div>'.'</td></tr>\n';
+	}
+
+	/* 在接收到送出要求後馬上檢查是否正確 */
+	public function autoHookRegistBegin(&$name, &$email, &$sub, &$com, $upfileInfo, $accessInfo){
+		if (adminAuthenticate('check') === true ) return; //no captcha for admin mode
+		$resp = $this->reCaptcha->verifyResponse($_SERVER['REMOTE_ADDR'], $_POST['g-recaptcha-response']);
+		if($resp == null || !$resp->success){ error('reCAPTCHA failed！You are not acting like a human!'); } // 檢查
+	}
 }
-?>
+
